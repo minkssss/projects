236bce56430173ce287a6c19e6daf8c587c8f1a4
minkssss <119494674+minkssss@users.noreply.github.com>
2022-12-01 15:43:04 +0700

    Update Data Transformation -- Penguin.R

--- R Programming/Data Transformation -- Penguin.R
+++ R Programming/Data Transformation -- Penguin.R
@@ -1,13 +1,14 @@
 ## read csv file from internet
 penguins <- read.csv("https://raw.githubusercontent.com/mwaskom/seaborn-data/master/penguins.csv")
 
-## basic data manipulation
-mean( penguins$bill_length_mm ) # result will show as NA so have to put more command
-mean( penguins$bill_length_mm, na.rm = TRUE) # can show mean
+## basic data manipulation -- quickly recheck NA values
+mean( penguins$bill_length_mm ) # If result show as NA, meaning that this data has NA values. 
+# put more command `na.rm = TRUE` for calculating
+mean( penguins$bill_length_mm, na.rm = TRUE) 
 sum( penguins$bill_length_mm, na.rm = TRUE)
 median( penguins$bill_depth_mm, na.rm = TRUE)
 
-summary(penguins)
+summary(penguins)  # This will represent all basic stats
 
 ## preview dataframe
 head(penguins, 3)
@@ -19,16 +20,14 @@ str(penguins) # check structure
 ## subset first 10 rows
 mini_penguins <- penguins[1:10, ]
 
-## create new column
+## create new column (will append at last column)
 mini_penguins$newcolumn <- mini_penguins$bill_length_mm * 2
 View(mini_penguins)
 
 ## drop column has 2 ways
-#1. assign null
+#1. assign null to that column
 mini_penguins$newcolumn <- NULL
-View(mini_penguins)
-
-#2. subset all except that col
+#2. subset all columns except that column
 mini_penguins <- mini_penguins[ , 1:6] 
 View(mini_penguins)
 
@@ -37,14 +36,14 @@ result <- subset(mini_penguins,
                  bill_length_mm > 40,
                  c("species", "island", "bill_length_mm"))
 
-## change col names
-names(result)  # use names() to view col name
+## change column names
+names(result)  # use command `names()` to view column names
 names(result) <- c("SPEC", "LAND", "BILL_LENGTH")
 
-## count missing values by row
-complete.cases(penguins)
+## count NA values by row
+complete.cases(penguins) # this will return TRUE or FALSE (TRUE == NA value)
 
-nrow(penguins) - sum(complete.cases(penguins))
+nrow(penguins) - sum(complete.cases(penguins)) 
 
-## filter only row that contain NA records
+## filter only row that contain NA values
 penguins[ !complete.cases(penguins), ] # ! change from TRUE to FALSE / FALSE to TRUE
